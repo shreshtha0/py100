9c58ce8588530a69d12088a169745ee4c2cbd8ae
SHRESHTHA CHAUDHARY <64724476+shreshtha0@users.noreply.github.com>
2021-04-10 17:02:20 +0530

    Add files via upload

---
+++ atm.py
@@ -0,0 +1,10 @@
+class Atm(object) :
+    def __init__(self,cardnumber,pinnumber):
+        self.cardnumber=cardnumber
+        self.pinnumber=pinnumber
+    
+     def cashWithdrawl():
+         print("Withdraw ur cash")
+
+transaction=Atm(123456,789012)
+print(transaction.cashWithdrawl())
